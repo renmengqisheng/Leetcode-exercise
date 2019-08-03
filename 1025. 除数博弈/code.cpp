fefe1c6b1547d33d8f5e9922fe810991b86cc80e
renmengqisheng <280947007@qq.com>
2019-08-03 14:32:08 +0800

    Update code.cpp

--- 1025. 除数博弈/code.cpp
+++ 1025. 除数博弈/code.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
     bool divisorGame(int N) {
-    /********思路一*********
-        if(N == 0 || N == 1)
-            return false;
-        
-        for(int i = 1; i < N; i++)
-        {
-            if(N % i == 0)
-                return !divisorGame(N-i);
-        }
-        return false;
-    **********************/
-    /********思路二*********/
         retrun (N%2) == 0;
     }
 };
